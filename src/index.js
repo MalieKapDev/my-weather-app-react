70a3a79f9366b1d892388afb2751c8e2dce5a1c9
Malie Kapelianis <maliekapelianis@gmail.com>
2024-05-27 14:28:30 +0200

    Edited page title, and added links to portfolio, github and netlify

--- src/index.js
+++ src/index.js
@@ -12,6 +12,23 @@ root.render(
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <p>
+        This project was coded by{" "}
+        <a href="https://maliekapdev-portfolio.netlify.app/" target="_blank">
+          Malie Kapelianis
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a href="https://github.com/MalieKapDev" target="_blank">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://my-weather-app-react-maliekapdev.netlify.app/"
+          target="_blank"
+        >
+          Netlify
+        </a>
+      </p>
     </div>
   </StrictMode>
 );
